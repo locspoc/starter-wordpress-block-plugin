193c86de3758c19d033ac083e0cdec2d43333e18
Loc Tran <mrlocspoc@gmail.com>
2023-08-11 21:16:43 +0930

    Initial commit

---
+++ build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '6f11a6eb6fd11954d7bc');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '39001a305e418b2d4d77');
